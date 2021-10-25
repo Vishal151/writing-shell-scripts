2458b2c55ed710f78eba32dc202c7e820860d183
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-10-25 13:26:00 +0100

    More flow control

---
+++ branchcase.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "Enter a number between 1 and 3 inclusive > " character
+case $character in
+    1 ) echo "You entered one."
+        ;;
+    2 ) echo "You entered two."
+        ;;
+    3 ) echo "You entered three."
+        ;;
+    * ) echo "You did not enter a number between 1 and 3."
+esac
+
+# Follows form:
+#   case word in
+#       patterns ) commands ;;
+#   esac
