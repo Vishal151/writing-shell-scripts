b7d1ac75e0c3f39cc994ee6c759dd75ec5d942de
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-10-25 14:09:24 +0100

    Adding positional parameters

--- sysinfo_page.sh
+++ sysinfo_page.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# sysinfo_page - A script to produce an HTML file
+# sysinfo_page - A script to produce a system information HTML file
 
 ##### Constants
 
@@ -14,7 +14,9 @@ system_info()
 {
     echo "<h2>System release info</h2>"
     echo "<p>Function not yet implemented</p>"
-}
+
+}   # end of system_info
+
 
 show_uptime()
 {
@@ -22,7 +24,9 @@ show_uptime()
     echo "<pre>"
     uptime
     echo "</pre>"
-}
+
+}   # end of show_uptime
+
 
 drive_space()
 {
@@ -30,36 +34,95 @@ drive_space()
     echo "<pre>"
     df
     echo "</pre>"
-}
+
+}   # end of drive_space
+
 
 home_space()
 {
     # Only the superuser can get this information
-    if [ "$(id -u)" = "0"]; then
+
+    if [ "$(id -u)" = "0" ]; then
         echo "<h2>Home directory space by user</h2>"
         echo "<pre>"
         echo "Bytes Directory"
-            du -s /home/* | sort -nr
+        du -s /home/* | sort -nr
         echo "</pre>"
     fi
-    
-}   # end of home space
+
+}   # end of home_space
+
+
+write_page()
+{
+    cat <<- _EOF_
+    <html>
+        <head>
+        <title>$TITLE</title>
+        </head>
+        <body>
+        <h1>$TITLE</h1>
+        <p>$TIME_STAMP</p>
+        $(system_info)
+        $(show_uptime)
+        $(drive_space)
+        $(home_space)
+        </body>
+    </html>
+_EOF_
+
+}
+
+usage()
+{
+    echo "usage: sysinfo_page [[[-f file ] [-i]] | [-h]]"
+}
+
 
 ##### Main
 
-cat <<- _EOF_
-  <html>
-  <head>
-      <title>$TITLE</title>
-  </head>
+interactive=
+filename=~/sysinfo_page.html
 
-  <body>
-      <h1>$TITLE</h1>
-      <p>$TIME_STAMP</p>
-      $(system_info)
-      $(show_uptime)
-      $(drive_space)
-      $(home_space)
-  </body>
-  </html>
-_EOF_
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )           shift
+                                filename=$1
+                                ;;
+        -i | --interactive )    interactive=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+
+# Test code to verify command line processing
+
+if [ "$interactive" = "1" ]; then
+
+    response=
+
+    read -p "Enter name of output file [$filename] > " response
+    if [ -n "$response" ]; then
+        filename="$response"
+    fi
+
+    if [ -f $filename ]; then
+        echo -n "Output file exists. Overwrite? (y/n) > "
+        read response
+        if [ "$response" != "y" ]; then
+            echo "Exiting program."
+            exit 1
+        fi
+    fi
+fi
+
+
+# Write page (comment out until testing is complete)
+
+# write_page > $filename
